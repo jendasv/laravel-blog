80ab95a745c9072a100e7404bdf1ee04833e0810
jenda <jan.svoboda@11teamsports.com>
2026-03-05 11:03:45 +0100

    testing stuff

--- src/app/Providers/AppServiceProvider.php
+++ src/app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -24,5 +25,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Paginator::useTailwind();
+        Blade::componentNamespace('Laravel\Breeze\View\Components', 'auth');
     }
 }
